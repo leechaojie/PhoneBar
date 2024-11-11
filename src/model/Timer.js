02e7eea177a70e3e114ae9037353332e1cbc85b0
lichaojie <li.chaojie@outlook.com>
2024-11-11 15:55:25 +0800

    🦄 refactor: 重构Timer类使用Web Worker实现后台计时

--- src/model/Timer.js
+++ src/model/Timer.js
@@ -7,7 +7,7 @@ class Timer extends EventEmitter {
     constructor(seconds = 0) {
         super();
         this.seconds = seconds;
-        this.intervalId = null;
+        this.worker = null;
     }
 
     /**
@@ -16,18 +16,7 @@ class Timer extends EventEmitter {
      * @returns {Timer} 当前对象
      */
     start() {
-        this._clearInterval(); // 避免重复启动
-        const startTime = Date.now() - this.seconds * 1000; // 基于当前秒数修正时间
-
-        // 立即触发一次 change 事件，推送当前时间
-        this.emit('change', this.seconds, this.format());
-
-        // 然后每秒触发更新
-        this.intervalId = setInterval(() => {
-            const currentTime = Date.now();
-            this.seconds = Math.floor((currentTime - startTime) / 1000);
-            this.emit('change', this.seconds, this.format()); // 更新 UI
-        }, 1000); // 每秒触发一次
+        this._initializeWorker(this.seconds);
         return this;
     }
 
@@ -37,30 +26,71 @@ class Timer extends EventEmitter {
      * @returns {Timer} 当前对象
      */
     stop() {
-        this._clearInterval();
+        if (this.worker) {
+            this.worker.terminate();
+            this.worker = null;
+        }
         this.seconds = 0;
         return this;
     }
 
     /**
      * 重启任务
+     *
+     * @param {number} seconds - 重新开始的秒数
+     * @returns {Timer} 当前对象
      */
     restart(seconds = 0) {
-        this.stop();
+        this.stop(); // 停止当前 Worker
         this.seconds = seconds;
-        this.start();
+        this.start(); // 重新启动 Worker
+        return this;
     }
 
     /**
-     * 清除定时器
+     * 初始化 Worker 并设置事件监听
      *
+     * @param {number} seconds - 起始秒数
      * @private
      */
-    _clearInterval() {
-        if (this.intervalId) {
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-        }
+    _initializeWorker(seconds) {
+        // 定义 Worker 的内容（相当于 timerWorker.js 的内容）
+        const workerCode = `
+            let seconds = 0;
+            let startTime = Date.now();
+
+            function updateTimer() {
+                const currentTime = Date.now();
+                seconds = Math.floor((currentTime - startTime) / 1000);
+                postMessage(seconds); // 向主线程发送秒数
+            }
+
+            // 监听主线程的重置命令
+            self.onmessage = (event) => {
+                if (event.data.action === 'restart') {
+                    seconds = event.data.seconds || 0;
+                    startTime = Date.now() - seconds * 1000; // 重设起始时间
+                }
+            };
+
+            setInterval(updateTimer, 1000); // 每秒发送一次更新
+        `;
+
+        // 创建 Blob URL
+        const blob = new Blob([workerCode], { type: "application/javascript" });
+        const workerUrl = URL.createObjectURL(blob);
+
+        // 创建 Worker 实例
+        this.worker = new Worker(workerUrl);
+
+        // 监听 Worker 发送的时间更新消息
+        this.worker.onmessage = (event) => {
+            this.seconds = event.data;
+            this.emit('change', this.seconds, this.format()); // 每次更新时触发 'change' 事件
+        };
+
+        // 向 Worker 发送初始秒数和重启指令
+        this.worker.postMessage({ action: 'restart', seconds });
     }
 
     /**
